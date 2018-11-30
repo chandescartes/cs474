2e793dbb382664c82721fa8e56424d3881368411
Ha JeongHo <hajeongho95@gmail.com>
2018-11-30 18:03:54 +0900

    first commit -> add readme, main, utils, updated requirement.txt

---
+++ main.py
@@ -0,0 +1,13 @@
+import utils
+import csv
+import os, sys
+
+
+corpus = utils.Corpus()
+
+
+with open('results.csv', 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',')
+    csvwriter.writerow(["baseball_" + i for i in ['entropy', 'tf-idf', 'tdv']] + ["hockey_" + i for i in ['entropy', 'tf-idf', 'tdv']])
+    for i in range(50):
+        csvwriter.writerow([result_baseball[0][i], result_baseball[1][i], result_baseball[2][i], result_hockey[0][i], result_hockey[1][i], result_hockey[2][i]])
